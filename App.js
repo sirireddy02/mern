d8690426f4297df6a41ab3c3e4653e34b5a7e3e9
sirireddy02 <158065636+sirireddy02@users.noreply.github.com>
2024-02-05 14:37:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>siri
+        <h2>2211cs010311</h2>
+        <h3>7A</h3>3
+      </h1>
+    </div>
+  );
+}
+
+export default App;
